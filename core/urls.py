23e87f2f9d1e7a71a5b4d876edb18ddfce1f5ef8
nguyenthetai123 <nguyenthetai.itdev97@gmail.com>
2023-01-23 23:12:36 +0700

    Initialize project using Create React App

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include, re_path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/blog/', include('apps.blog.urls')),
+    path('api/category/', include('apps.category.urls'))
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += [re_path(r'^.*',
+                        TemplateView.as_view(template_name='index.html'))]
